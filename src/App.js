25e1b72ba322b1b8550c8ab1d16c869a45eab482
aubepluieh3 <aubepluieh3@naver.com>
2022-10-04 17:24:21 +0900

    #1.4 Animations and Pseudo Selectors Part Two

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ const rotationAnimation = keyframes`
   }
 `;
 
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
 const Box = styled.div`
   height: 200px;
   width: 200px;
@@ -26,14 +30,8 @@ const Box = styled.div`
   justify-content: center;
   align-items: center;
   animation: ${rotationAnimation} 1s linear infinite;
-  span {
-    font-size: 36px;
-    &:hover {
-      font-size: 48px;
-    }
-    &:active {
-      opacity: 0;
-    }
+  ${Emoji}:hover {
+    font-size: 98px;
   }
 `;
 
@@ -41,8 +39,9 @@ function App() {
   return (
     <Wrapper>
       <Box>
-        <span>🤩</span>
+        <Emoji>🤩</Emoji>
       </Box>
+      <Emoji>😡</Emoji>
     </Wrapper>
   );
 }
