c0cf5429f8f02f901781a314102757ae9c0a1a05
aubepluieh3 <aubepluieh3@naver.com>
2022-10-05 21:51:10 +0900

    #2.0 DefinitelyTyped

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 const darkTheme = {
   textColor: "whitesmoke",
   backgroundColor: "#111",
@@ -11,13 +10,14 @@ const darkTheme = {
 
 const lightTheme = {
   textColor: "#111",
-  backgroundColor: "#whitesmoke",
+  backgroundColor: "whitesmoke",
 };
 
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
