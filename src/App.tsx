8c950e0707419a9a2f60aed33810fa6f00dd1eff
aubepluieh3 <aubepluieh3@naver.com>
2022-10-06 19:48:46 +0900

    #2.5 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
 import React, { useState } from "react";
-function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
+import styled from "styled-components";
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+function App() {
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Protected</H1>
+    </Container>
   );
 }
 
