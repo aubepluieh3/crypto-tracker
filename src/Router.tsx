1ba845bcf2a9ca1035dfcc60178ba72a47e56053
soo <aubepluieh3@naver.com>
2022-10-18 21:18:11 +0900

    routerFix

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,19 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
 
-interface IRouterProps {
+interface IRouterProps {}
 
-}
-
-function Router({ }: IRouterProps) {
+function Router({}: IRouterProps) {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path=":coinId/*" element={<Coin />}></Route>
         <Route path="/" element={<Coins />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
